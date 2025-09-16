f6bc93ec6f3d9cc46475c24259b6ffed9e48a2c1
trotacodigos <82924238+trotacodigos@users.noreply.github.com>
2025-09-16 09:14:08 +0000

    Deploying to gh-pages from @ trotacodigos/trotacodigos.github.io@292cc77c8f9c9b8e84b935e69e4f54c7d8d1dff4 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "trotacodigos/ahriikim",
+    "data-repo": "trotacodigos/trotacodigos.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
